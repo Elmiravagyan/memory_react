32f278516c11c5b42c29a5f3fe91e594bef415b3
HaykKeshishyan <hkeshishyan94@gmail.com>
2022-01-20 13:26:16 +0400

    second commit

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,60 +1,50 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Card from './Card/Card.jsx'
 import { useState } from 'react'
+import axios from "axios";
 
 const Cards = (props) => {
-    const [items, setItems] = useState([
-        { id: "1", img: "/img/1.png", state: "" },
-        { id: "1", img: "/img/1.png", state: "" },
-        { id: "2", img: "/img/2.png", state: "" },
-        { id: "2", img: "/img/2.png", state: "" },
-        { id: "3", img: "/img/3.png", state: "" },
-        { id: "3", img: "/img/3.png", state: "" },
-        { id: "4", img: "/img/4.png", state: "" },
-        { id: "4", img: "/img/4.png", state: "" },
-        { id: "5", img: "/img/5.png", state: "" },
-        { id: "5", img: "/img/5.png", state: "" },
-        { id: "6", img: "/img/6.png", state: "" },
-        { id: "6", img: "/img/6.png", state: "" },
-        { id: "7", img: "/img/7.png", state: "" },
-        { id: "7", img: "/img/7.png", state: "" },
-        { id: "8", img: "/img/8.png", state: "" },
-        { id: "8", img: "/img/8.png", state: "" },
-        { id: "9", img: "/img/9.png", state: "" },
-        { id: "9", img: "/img/9.png", state: "" },
-        { id: "10", img: "/img/10.png", state: "" },
-        { id: "10", img: "/img/10.png", state: "" },
-    ].sort(() => Math.random() - 0.5))
+    const [items, setItems] = useState([])
 
-    const [prev, setPrev] = useState(-1)
+    const [prev, setPrev] = useState(null)
+    const [fliped, setFliped] = useState(false)
+
+    React.useEffect(() => {
+        async function fetchData() {
+            const cartData = await axios.get('https://61b8b44138f69a0017ce5cd7.mockapi.io/Memory_game');
+            setItems(cartData.data.sort(() => Math.random() - 0.5));
+        }
+            fetchData();
+    }, [])
 
     function check (current) {
         if(items[current].id == items[prev].id){
             items[current].state = "correct"
             items[prev].state = "correct"
-            console.log(items[current]);
-            console.log(items[prev]);
-            setPrev(-1)
+            setPrev(null)
         }else{
             items[current].state = "wrong"
             items[prev].state = "wrong"
-            console.log(items[current]);
-            console.log(items[prev]);
             setItems([...items])
+            setFliped(true)
             setTimeout(() => {
                 items[current].state = ""
                 items[prev].state = ""
                 setItems([...items])
+                setPrev(null)
+                setFliped(false)
             }, 1000)
         }
     }
     function flipCard(id){
-        if(prev === -1) {
-            items[id].state = "active"
-            setItems([...items])
-            setPrev(id)
-        } else {
-            check(id)
+        if(!fliped) {
+            if(prev === null) {
+                items[id].state = "active"
+                setItems([...items])
+                setPrev(id)
+            } else {
+                check(id)
+            }
         }
     }
 
